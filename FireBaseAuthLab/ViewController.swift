a776d548503a1e06c911749e4de0bd09de4e524d
melinda <melindad_82@msn.com>
2020-03-05 17:35:38 -0500

    initial commit

---
+++ FireBaseAuthLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireBaseAuthLab
+//
+//  Created by Melinda Diaz on 3/5/20.
+//  Copyright © 2020 Melinda Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
