9718fb7307d8f0276b4616ac42e5f70778cfff14
melinda <melindad_82@msn.com>
2020-03-05 18:13:19 -0500

    added cocoa pods

--- FireBaseAuthLab/AppDelegate.swift
+++ FireBaseAuthLab/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+// Configuring Firebase step 1
+import Firebase
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+         FirebaseApp.configure()
         return true
     }
 
